7d49ab5783060c6be60be494e33892e14c5b8807
Johan Alexander Paniagua Bedoya <japaniaguab@unal.edu.co>
2022-01-05 11:29:37 -0500

    finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CabeceroComponent } from './cabecero/cabecero.component';
+import { EgresoComponent } from './egreso/egreso.component';
+import { IngresoComponent } from './ingreso/ingreso.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { IngresoServicio } from './ingreso/ingreso.servicio';
+import { EgresoServicio } from './egreso/egreso.servicio';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent,
+    EgresoComponent,
+    IngresoComponent,
+    FormularioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    IngresoServicio,
+    EgresoServicio
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
